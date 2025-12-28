65da16e2abe1a2ed981a480602bf4c5c8680314f
Diwash Bhattarai <diwashb999@gmail.com>
2025-12-28 18:50:39 +0545

    ci(fix): add prisma generate task before typecheck

--- packages/jest-config/src/nest.ts
+++ packages/jest-config/src/nest.ts
@@ -8,7 +8,12 @@ export const nestConfig = {
 	transform: {
 		"^.+\\.(t|j)s$": "ts-jest",
 	},
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/$1",
+	},
+	modulePaths: ["<rootDir>"],
 	collectCoverageFrom: ["**/*.(t|j)s"],
+	coveragePathIgnorePatterns: ["node_modules", "generated", "dist", "runtime", ".*\\.prisma"],
 	coverageDirectory: "../coverage",
 	testEnvironment: "node",
 } as const satisfies Config;
